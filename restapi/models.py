aa75cf49f35cc560b34bf54866971d3710d5a94e
kipkirui koech <106087508+kipkirui88@users.noreply.github.com>
2023-06-13 06:31:58 +0300

    models
    modify models.py

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class students(models.Model):
+    adm = models.IntegerField()
+    name = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
