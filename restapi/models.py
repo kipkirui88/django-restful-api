8b8a0890e5fc2f6c08f3b9d9a9e82747123b8e96
kipkirui koech <106087508+kipkirui88@users.noreply.github.com>
2023-06-13 10:44:40 +0300

    initial coding
    add models,views,urls serializers, update settings and run migrations

--- restapi/models.py
+++ restapi/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class students(models.Model):
+class Student(models.Model):
     adm = models.IntegerField()
     name = models.CharField(max_length=100)
     course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
